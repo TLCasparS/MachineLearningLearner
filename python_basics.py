bd238b78143ad9c610cffb4920d94e2c0c34b07d
TLCasparS <112706704+TLCasparS@users.noreply.github.com>
2023-05-09 12:40:20 +0200

    Update python_basics.py

--- python_basics.py
+++ python_basics.py
@@ -3,5 +3,11 @@ from pandas import DataFrame
 
 data = pandas.read_csv('lsd_math_score_data.csv')
 
+data["high_scores"] = data["Avg_Math_Test_Score"] +100
+data["high_scores"] = data["high_scores"]**2
+data
 
+columnList = ["LSD_ppm", "Avg_Math_Test_Score"] # list erstellen
 
+cleanData = data[columnList] # liste als Schablone für neuen DataFrame benutzen
+print(cleanData)
